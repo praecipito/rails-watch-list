c78928d7afd6b0ae884b3c14c3525f655ae08421
Caio Alves <caioalvesfig@gmail.com>
2023-02-27 13:58:56 -0300

    models ok

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  # before_destroy :check_for_bookmarks
+
+  validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :overview, presence: true
+
+  # private
+
+  # def check_for_bookmarks
+  #   if bookmarks.count > 0
+  #     errors.add_to_base("This movie has bookmarks. It can't be deleted.")
+  #     false
+  #   end
+  # end
+end
